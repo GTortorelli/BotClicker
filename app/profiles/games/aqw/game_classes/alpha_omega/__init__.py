4b94c1ddf2ec07643cc9557f0040424f305e9b20
GTortorelli <gustavo.dra.tor@hotmail.com>
2020-10-19 15:21:02 -0300

    creating profiles

---
+++ app/profiles/games/aqw/game_classes/alpha_omega/__init__.py
@@ -0,0 +1,20 @@
+from app.mouse.model import Mouse
+
+
+class AlphaOmega:
+    mouse = Mouse()
+
+    def rank1(self):
+        pass
+
+    def rank2(self):
+        pass
+
+    def rank3(self):
+        pass
+
+    def rank4(self):
+        pass
+
+    def rank5(self):
+        pass
