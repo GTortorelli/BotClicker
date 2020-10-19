cdc84f2a33cd755abc764ea5555605a5b79e1f5b
GTortorelli <gustavo.dra.tor@hotmail.com>
2020-10-19 16:59:57 -0300

    implemmenting profiles and clicker functions

--- app/mouse/__init__.py
+++ app/mouse/__init__.py
@@ -1,9 +1,12 @@
-import pyautogui as mouse_commands
+import pyautogui
+
+mouse_commands = pyautogui
 
 def get_current_mouse_position() -> dict:
     """Gets the coordinates os the mouse on screen
        return format:
       {"X": currentMouseY, "Y": currentMouseY}
     """
-    current_mouse_x, current_mouse_y = mouse_commands.position()
-    return {"X": f"{current_mouse_x}", "Y": f"{current_mouse_x}"}
+    current_mouse_x = mouse_commands.position().x
+    current_mouse_y = mouse_commands.position().y
+    return {"X": f"{current_mouse_x}", "Y": f"{current_mouse_y}"}
--- app/profiles/games/aqw/game_classes/alpha_omega/__init__.py
+++ app/profiles/games/aqw/game_classes/alpha_omega/__init__.py
@@ -1,17 +1,39 @@
-from app.mouse.model import Mouse
+from app.keyboard import keyboard_commands
+from app.mouse import mouse_commands
 
 
 class AlphaOmega:
-    mouse = Mouse()
 
     def rank1(self):
-        pass
+        """Farming script for Alpha omega classes in rank 1"""
+        print('Make sure youre close enought from enemy')
+        continue_ = True
+        while continue_:
+            continue_ = not keyboard_commands.is_pressed('f6')
+
+        while not continue_:
+            mouse_commands.click(760, 959)
+            mouse_commands.click(835, 981)
+            continue_ = keyboard_commands.is_pressed('f6')
 
     def rank2(self):
-        pass
+        """Farming script for Alpha omega classes in rank 2"""
+        print('Make sure youre close enought from enemy')
+        while keyboard_commands.is_pressed("f6"):
+            mouse_commands.click(760, 960, interval=1)
+            mouse_commands.click(835, 960, interval=1)
+            mouse_commands.click(910, 960, interval=1)
+            mouse_commands.click(852, 137, interval=1)
 
     def rank3(self):
-        pass
+        """Farming script for Alpha omega classes in rank 3"""
+        print('Make sure youre close enought from enemy')
+        while keyboard_commands.is_pressed("f6"):
+            mouse_commands.click(760, 960, interval=1)
+            mouse_commands.click(835, 960, interval=1)
+            mouse_commands.click(910, 960, interval=1)
+            mouse_commands.click(852, 137, interval=1)
+            mouse_commands.click(985, 960, interval=1)
 
     def rank4(self):
         pass
